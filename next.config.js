444eef60a039135b6ecafc2884f5708bb60eaedd
Kamgang Nintcheu David <dkamgangnin@enseirb-matmeca.fr>
2023-12-31 02:58:07 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-// next.config.js
-module.exports = {
-    basePath: '/Flopsky.github.io',
-    // other configurations...
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+
+module.exports = nextConfig
