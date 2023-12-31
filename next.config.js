eb46134b5543506f3c49db69a53c53c0f7ef401d
David K <75791840+Flopsky@users.noreply.github.com>
2023-12-31 12:34:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-// next.config.js
-const withImages = require('next-images');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withImages({
-    basePath: '/Flopsky.github.io',
-    // Your other configurations...
-
-    // Use the target: 'serverless' option to enable the export feature
-    target: 'serverless',
-
-    // Add the following lines for static HTML export
-    exportPathMap: async function () {
-        return {
-            '/': { page: '/' },
-            // Add other pages as needed
-        };
-    },
-});
+module.exports = nextConfig
