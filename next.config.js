e5e104c205878f2db633c2fdf45728d3dad6bb85
Kamgang Nintcheu David <dkamgangnin@enseirb-matmeca.fr>
2023-12-31 02:41:31 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    basePath: '/Flopsky.github.io',
+    // other configurations...
+};
