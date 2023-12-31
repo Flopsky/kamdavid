f1a5bf4a927b39498dfaef843f7a1f6c18416e72
David K <75791840+Flopsky@users.noreply.github.com>
2023-12-31 17:28:21 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Image from 'next/image'
 import { Component } from '@/components/component'
 
+export const metadata = {
+  title: "David KAMGANG",
+  description: "Hello this is my page",
+}
+
 export default function Home() {
   return (
     Component()
