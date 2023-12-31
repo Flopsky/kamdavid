e62665c04dd9ea86c4630d7093c94abfee032f77
Kamgang Nintcheu David <dkamgangnin@enseirb-matmeca.fr>
2023-12-31 02:54:02 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 // next.config.js
 module.exports = {
     basePath: '/Flopsky.github.io',
-    // Your other configurations...
-
-
-    async generateStaticParams() {
-        // Return the paths you want to export
-        return {
-            '/': { page: '/' },
-            // Add other pages as needed
-        };
-    },
+    // other configurations...
 };
