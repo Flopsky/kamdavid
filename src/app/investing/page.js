ac8057f71dbae09b2c71aa6cd9e69166cca1077d
David K <75791840+Flopsky@users.noreply.github.com>
2024-01-02 12:29:46 +0100

    Create page.js

---
+++ src/app/investing/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Component } from '@/components/investing'
+
+export const metadata = {
+    title: "Hello this is my page",
+    description: "**",
+}
+
+export default function Home() {
+    return (
+        Component()
+    )
+}
