ec7b71ebd65ca47b4c358fcb65d0f254c0c83eaf
David K <75791840+Flopsky@users.noreply.github.com>
2024-01-15 23:49:03 +0100

    Update component.jsx

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -127,7 +127,7 @@ export function Component() {
                   <p className="text-sm text-gray-300">
                     This project goal is to provide dynamic dashboards on the state of stocks. It helps answer the question : should that hold or sell ? LLM, GCP, Pandas, Streamlit, Chart Js, Github...
                     <a href="https://www.kamdavid.com/investing" rel="noopener noreferrer" target="_blank">
-                      Clic me
+                      <p className="text-sm text-blue-300">Clic me</p>
                   </a>
                   </p>
                 </CardContent>
