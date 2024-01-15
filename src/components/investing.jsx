49c7515a124cbe5854240ca0a528966d0c7fc99e
David K <75791840+Flopsky@users.noreply.github.com>
2024-01-15 23:52:21 +0100

    Update investing.jsx

--- src/components/investing.jsx
+++ src/components/investing.jsx
@@ -2423,7 +2423,7 @@ export function Component() {
                 </Card>
 
                 <div >
-                    <h1>The bigger the better. This leaderboard is updated every 24h</h1>
+                    <h1>The greener the better, The bigger the better. This leaderboard is updated every 24h</h1>
                     <InteractiveTreemap data={data} />
                 </div>
 
