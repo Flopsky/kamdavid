033733913ed95919650ce56be2450dd884d3b742
David K <75791840+Flopsky@users.noreply.github.com>
2024-01-17 13:05:00 +0100

    Update investing.jsx

--- src/components/investing.jsx
+++ src/components/investing.jsx
@@ -2696,7 +2696,7 @@ export function Component() {
                     </CardHeader>
                     <CardContent>
                         <p className="text-sm text-gray-300">
-                            This project goal is to provide dynamic dashboards on the state of stocks. It helps answer the question : should that hold or sell ? LLM, GCP, Pandas, Streamlit, Chart Js, Github
+                            This project goal is to provide dynamic dashboards on the state of stocks. It helps answer the question : should I hold or sell or buy ? LLM, GCP, Pandas, Streamlit, Chart Js, Github
                         </p>
                     </CardContent>
                 </Card>
