8f9f5648a94adee03994dd693fcc30399c724064
David K <75791840+Flopsky@users.noreply.github.com>
2024-02-04 21:20:54 +0100

    Update component.jsx

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -13,7 +13,7 @@ export function Component() {
       <div className="bg-gray-900 min-h-screen flex flex-col">
         <header
           className="w-full flex flex-wrap justify-between items-center p-4 sticky top-0 bg-gray-900 z-50">
-          <h1 className="text-4xl font-bold">David KAMGANG  </h1>
+          <h1 className="text-4xl font-bold">David KAMGANG NINTCHEU </h1>
           <nav className="flex flex-wrap gap-4">
             <Link className="text-lg font-medium hover:underline" href="/">
               <InfoIcon className="w-4 h-4 text-blue-500" />
